a3d9bb636e5f7cdf7a0c0b68ea7f5c686be54311
Amanda Wehrman <arwehrman@gmail.com>
2017-11-06 18:11:42 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if !query.empty?
+      self.all.collect do |student|
+      student if student.name.downcase.include?query.downcase
+    end.compact
+    else
+      self.all
+    end
+  end
 end
